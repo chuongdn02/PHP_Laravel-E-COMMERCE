dc276296986f6c0f93b6a274b32a192f7032f1a4
chuongdn02 <chuongnguyendn02@gmail.com>
2023-11-26 01:14:30 +0700

    day2.2

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -65,9 +65,9 @@
   <!-- /.content-wrapper -->
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
+      <!-- <b>Version</b> 3.2.0 -->
     </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <!-- <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. -->
   </footer>
   </div>
   @include('admin.footer')
--- resources/views/admin/menu/add.blade.php
+++ resources/views/admin/menu/add.blade.php
@@ -1,5 +1,52 @@
 @extends('admin.main')
 
 @section('content')
-  Nội dung add
+  <form action="" method="POST">
+    <div class="card-body">
+
+      <div class="form-group">
+        <label for="menu">Tên Danh Mục</label>
+        <input type="text" name="menu" class="form-control" id="menu" placeholder="Enter name">
+      </div>
+
+      <div class="form-group">
+        <label for="menu">Danh Mục</label>
+        <select class="form-control" name="parent_id" >
+          <option value="0">Danh Mục Cha</option>
+          <option value="1">SamSung</option>
+        </select>
+      </div>
+
+      <div class="form-group">
+        <label >Mô tả</label>
+        <textarea name="description" class="form-control" ></textarea>
+      </div>
+
+      <div class="form-group">
+        <label >Mô tả chi tiết</label>
+        <textarea name="content" class="form-control" ></textarea>
+      </div>
+        
+        <div class="form-group">
+        <label >Kích hoạt</label>
+        <div class="custom-control custom-radio">
+          <input class="custom-control-input" value="1" type="radio" id="active" name="active" checked="">
+          <label for="active" class="custom-control-label">có</label>
+        </div>
+        <div class="custom-control custom-radio">
+          <input class="custom-control-input" value="" type="radio" id="no_active" name="active" >
+          <label for="no_active" class="custom-control-label">Không</label>
+        </div>
+      </div>
+    </div>
+
+       
+
+      
+
+
+    <div class="card-footer">
+      <button type="submit" class="btn btn-primary">Tạo Danh Mục</button>
+    </div>
+  </form>
 @endsection
